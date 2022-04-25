e29702175d7d4026c4cbc7c345d8d98ef9a67e2b
unknown <iburuchenko@gmail.com>
2022-04-25 14:20:30 +0300

    init with splash screen

---
+++ app/src/main/java/com/example/filmslist/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.filmslist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_FilmsList)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

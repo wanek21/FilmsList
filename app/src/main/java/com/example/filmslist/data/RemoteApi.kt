47bc3c1ac833575018a733ee35868239c6ec51c0
Ivan <iburuchenko@gmail.com>
2022-04-26 13:35:54 +0300

    fixed api key in gradle

--- app/src/main/java/com/example/filmslist/data/RemoteApi.kt
+++ app/src/main/java/com/example/filmslist/data/RemoteApi.kt
@@ -1,11 +1,12 @@
 package com.example.filmslist.data
 
+import com.example.filmslist.BuildConfig
 import com.example.filmslist.dto.JSONResponse
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface RemoteApi {
 
-    @GET("/svc/movies/v2/reviews/all.json?api-key=FdyNIk29N6orhkzuNNwWKg8Ny9Hpw2x9")
+    @GET("/svc/movies/v2/reviews/all.json?api-key=${BuildConfig.API_KEY}")
     fun getFilms(): Call<JSONResponse>
 }
